f8176a1281a8c7ad78221f3ace2c457fa8969d8d
luka4o4 <mordilukamait@gmail.com>
2026-02-21 11:01:55 +0100

    Added Python script for automated allow list update

---
+++ file-update-automation/file_update_script.py
@@ -0,0 +1,17 @@
+import_file = "allow_list.txt"
+
+remove_list = ["192.168.1.10", "10.0.0.5"]
+
+with open(import_file, "r") as file:
+    ip_addresses = file.read()
+
+ip_addresses = ip_addresses.split()
+
+for element in remove_list:
+    if element in ip_addresses:
+        ip_addresses.remove(element)
+
+ip_addresses = "\n".join(ip_addresses)
+
+with open(import_file, "w") as file:
+    file.write(ip_addresses)
